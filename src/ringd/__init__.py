0df8698c5cabd60627f37f6b8edddfc4ba053ed5
Jim Dixon <jddixon@gmail.com>
2017-07-26 11:48:24 -0700

    rewrite setup.py, MANIFEST.in; edit cfg files

--- src/ringd/__init__.py
+++ src/ringd/__init__.py
@@ -18,8 +18,8 @@ __all__ = ['__version__', '__version_date__', 'BUFSIZE',
            ]
 
 # - exported constants ----------------------------------------------
-__version__ = '0.3.9'
-__version_date__ = '2017-06-21'
+__version__ = '0.3.10'
+__version_date__ = '2017-07-26'
 BUFSIZE = 64 * 1024   # must be big enough for all using protocols
 
 # -------------------------------------------------------------------
